14e226dfc9497ff2cd93305838248929d9a9f228
avilag <avilag@minerva.cse.chalmers.se>
2026-03-31 23:45:14 +0200

    Problem 1

--- assigment1_problem1.sh
+++ assigment1_problem1.sh
@@ -1,1 +1,36 @@
 #!/bin/bash
+
+echo "System information"
+
+echo "Hostname: $(hostname)"
+
+echo "CPU details:"
+
+lscpu
+echo
+
+
+echo "System RAM"
+grep MemTotal /proc/meminfo
+echo
+
+echo "GPU details"
+nvidia-smi
+echo
+
+
+echo "Filesystem"
+df -T /data
+echo "Disk usage"
+df -h /data
+
+echo
+
+echo "linux kernel details"
+lsb_release -a
+
+echo
+echo "Python3 details"
+which python
+python3 --version
+
