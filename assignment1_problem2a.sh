69e28fa846cbb7c5eb6dc9a11271a1dc11f3f9c7
Gaby <gaby_castillo99@hotmail.com>
2026-04-04 18:39:49 +0200

    apptainer 2a

--- assignment1_problem2a.sh
+++ assignment1_problem2a.sh
@@ -3,15 +3,18 @@ echo "System information"
 echo "Hostname: $(hostname)"
 
 source ~/miniforge3/bin/activate
-apptainer run /data/courses/2026_dat471_dit066/containers/assignment1.sif  
 
-echo
+CONTAINER_IMAGE="/data/courses/2026_dat471_dit066/containers/assignment1.sif"
+
+
 echo "kernel version:"
-uname -r
+echo "Container image: "$CONTAINER_IMAGE" uname -r
+echo
+
+
 
 echo "CPU model"
-lscpu | grep "Model name:"
+apptainer exec "$CONTAINER_IMAGE" bash -c "lscpu | grep 'Model name:'"
 
 echo "Python3 details:"
-which python
-python3 --version
+apptainer exec "$CONTAINER_IMAGE" bash -c python3 --version
